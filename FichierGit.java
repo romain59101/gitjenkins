3c1ed700727254b428294070a869647fcf3eaa5d
unknown <you@example.comromain.surrans@gmail.com>
2019-06-18 12:22:54 +0200

    FichierGit

--- FichierGit.java
+++ FichierGit.java
@@ -1,6 +1,6 @@
 public class FichierGit {
     public static void main(String[] args) {
-        System.println("à partir de GIT");
-        
+        System.out.println("à partir de GIT");
+
     }
 }
